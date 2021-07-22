e27e991b7bd3c388e85eddec6fa523854f1158e2
pranavfiji <69599542+pranavfiji@users.noreply.github.com>
2021-07-21 20:45:16 -0400

    Add files via upload

---
+++ number.py
@@ -0,0 +1,11 @@
+number=int(input("Guess the correct number"))
+
+while chances<10:
+    if guess=number:
+        print("You are soo good, congratulations")
+        break
+
+    if not chances<10:
+        print("You lost try again")
+
+
